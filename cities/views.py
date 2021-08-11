dd47502ad653fec5c3313159cea3ec55db174c9d
Nikolai Glushchenko <63996222+NikolaiGL@users.noreply.github.com>
2021-08-11 14:23:27 +0300

    after 31st lesson

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from cities.forms import HtmlForm, CityForm
+from cities.models import City
+
+__all__ = (
+    'home', 'CityDetailView'
+)
+
+
+def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+    # if pk:
+        # city = City.objects.filter(id=pk).first()
+        #
+        # context = {'object': city}
+        # return render(request, 'cities/detail.html', context)
+    form = CityForm()
+    qs = City.objects.all()
+    context = {'objects_list': qs, 'form': form}
+    return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
